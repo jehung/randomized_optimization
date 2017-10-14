c792cb6eb40daa32b1caa939cf5f0a588443bd9d
jehung <jehung@me.com>
2017-10-14 12:09:09 -0700

    modify RHC on nn problem

--- NN1.py
+++ NN1.py
@@ -25,7 +25,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-#jp.startJVM(jp.getDefaultJVMPath(), "-ea")
+
 
 jp.startJVM(jp.getDefaultJVMPath(), '-ea', '-Djava.class.path=/Users/jennyhung/MathfreakData/School/OMSCS_ML/Assign2/abagail_py/ABAGAIL/ABAGAIL.jar')
 jp.java.lang.System.out.println("hello world")
